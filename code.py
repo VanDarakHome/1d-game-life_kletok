1c697674255f5bcb1ea0727bac322fd89c93011e
VanDarakHome <150126102+VanDarakHome@users.noreply.github.com>
2024-03-24 18:47:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,13 @@
 import time
 print("Привет, это игра 1D жизнь клеток, с правилами можно ознакомится в README")
 print("Введи длину строки")
-string_len = input()
+string_len = input() # Вводим длину строки
+# ---------------------------------
 while not string_len.isdigit():
-    print("Введи число")
-    string_len = input()
+    print("Введи число")               # Блок отвечающий за то что вводится длина строки
+    string_len = input()               # превращающий строку в число
 string_len = int(string_len)
+# ----------------------------------
 line = ['*'] * string_len
 print("Теперь введите саму строку")
 string = input()
@@ -13,7 +15,43 @@ while len(string) != len(line):
     print("А вам не кажется уважаемый игрок, что если длина строки", len(line), 'символов(так вы указали),')
     print("то длина строки не может быть равна", len(string), '(если что это длина строки, которую вы ввели)')
     string = input()
-for i in range(string_len):
-    if string[i] == ' ':
+for i in range(string_len):          # Блок отвечающий за ввод самой строки с последующий превращением в символы
+    if string[i] == ' ':             # Пример: fgр г ю   Шифр: *** * *
         line[i] = ' '
-print(line)
+# ---------------------------------
+print("Введите количество поколений")
+pokoleniya = input()
+while not pokoleniya.isdigit():      # Ввод поколений
+    print("Введи число")
+    pokoleniya = input()
+pokoleniya = int(pokoleniya)
+# ----------------------------------
+for i in line:
+    print(i, end="")
+print()                                          # Подготовка к следующему поколению
+line.append(line[0])
+x = line # Копия строки
+line = x[:-1] # все эл. кроме последнего
+# -----------------------------------
+for i in range(pokoleniya):
+    for j in range(string_len):
+        x2 = 0
+        if line[j - 1] == "*":
+            x2 += 1
+        if x[j + 1] == "*":
+            x2 += 1
+        if x[j] == "*":
+            if x2 != 1:
+                x[j] = " "
+        else:
+            if x2 == 1:                          # Работа самих поколений
+                x[j] = "*"
+    line = x[:-1]
+    x[-1] = x[0]
+    for j in line:
+        print(j, end="")
+    print()
+    time.sleep(0.5)
+# x - Копия строки x2 - счётчик
+# line - строка вывод на экран
+# j - элемент строки
